02d131848e2e4830d17b2797cdfaf7b6f16b7cbc
Txart Urzainki <iurzainki@gmail.com>
2019-12-07 09:30:59 +0200

    new plots

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ can_rst_fn = preprocessed_datafolder + r"/canals_clip.tif"
 #land_use_rst_fn = preprocessed_datafolder + r"/Landcover2017_clip.tif" # Not used
 peat_depth_rst_fn = preprocessed_datafolder + r"/Peattypedepth_clip.tif" # peat depth, peat type in the same raster
 #params_fn = r"/home/inaki/GitHub/dd_winrock/data/params.xlsx" # Luke
-#params_fn = r"/home/txart/Programming/GitHub/dd_winrock/data/params.xlsx" # home
-params_fn = r"/homeappl/home/urzainqu/dd_winrock/data/params.xlsx" # CSC
+params_fn = r"/home/txart/Programming/GitHub/dd_winrock/data/params.xlsx" # home
+#params_fn = r"/homeappl/home/urzainqu/dd_winrock/data/params.xlsx" # CSC
 
 
 if 'CNM' and 'cr' and 'c_to_r_list' not in globals():
@@ -114,7 +114,7 @@ n_canals = len(c_to_r_list)
 # HANDCRAFTED WATER LEVEL IN CANALS. CHANGE WITH MEASURED, IDEALLY.
 oWTcanlist = [x - CANAL_WATER_LEVEL for x in srfcanlist]
 
-hand_made_dams = False # compute performance of cherry-picked locations for dams.
+hand_made_dams = True # compute performance of cherry-picked locations for dams.
 """
 MonteCarlo
 """
@@ -125,6 +125,7 @@ for i in range(0,N_ITER):
     if hand_made_dams:
         # HAND-MADE RULE OF DAM POSITIONS TO ADD:
         hand_picked_dams = (11170, 10237, 10514, 2932, 4794, 8921, 4785, 5837, 7300, 6868)
+        hand_picked_dams = [6122, 6623, 1882, 5228, 9132, 7436, 11098, 9886, 7287, 841, 6293, 6908, 8442, 7648, 11043, 783, 11091, 2408, 9775, 426, 11225, 5303, 9759, 6945, 4373, 3305, 547, 1547, 402, 9305, 3482, 6947, 8698, 10001, 9983, 6999, 2832, 9022, 7884, 2007]
         damLocation = hand_picked_dams
     
     wt_canals = utilities.place_dams(oWTcanlist, srfcanlist, BLOCK_HEIGHT, damLocation, CNM)
--- output/plot.py
+++ output/plot.py
@@ -16,51 +16,43 @@ plt.close('all')
  Read
 """
 
-fname_mc = r'results_mc_2.txt'
-fname_ga = r'results_ga.txt'
-fname_sa = r'results_sa_2.txt'
+fname_mc = r'results_mc_3.txt'
+fname_ga = r'results_ga_3.txt'
+fname_sa = r'results_sa_3.txt'
 
 colnames = ['i', 'dry_peat_vol', 'ndams', 'niter', 'days', 'day_week', 'month', 'day_month', 'time', 'yr', 'water_changed_canals' ]
-rename_cols_sa = {'i':'dry_peat_vol', 'dry_peat_vol':'ndams', 'ndams':'niter', 'niter':'days', 'days':'day_week', 'day_week':'month', 'month':'day_month', 'day_month':'time', 'time':'yr' }
-colnames_ga = {'dry_peat_vol', 'ndams', 'niter', 'days', 'day_week', 'month', 'day_month', 'time', 'yr', 'blocks', 'a', 'b', 'c'}
-rename_cols_ga = {'day_month':'dry_peat_vol', 'niter':'ndams', 'c': 'niter', 'b':'days', 'blocks':'day_week', 'day_week':'month', 'days':'day_month', 'month':'time', 'ndams':'yr', 'a': 'b1', 'time':'b2', 'yr':'b3', 'dry_peat_vol':'b4'}
+colnames_sa = ['dry_peat_vol', 'ndams', 'niter', 'days', 'day_week', 'month', 'day_month', 'time', 'yr' ]
+colnames_ga = ['dry_peat_vol', 'ndams', 'niter', 'days', 'day_week', 'month', 'day_month', 'time', 'yr' ]
+#rename_cols_ga = {'day_month':'dry_peat_vol', 'niter':'ndams', 'c': 'niter', 'b':'days', 'blocks':'day_week', 'day_week':'month', 'days':'day_month', 'month':'time', 'ndams':'yr', 'a': 'b1', 'time':'b2', 'yr':'b3', 'dry_peat_vol':'b4'}
 
-mc_df = pd.read_csv(fname_mc, delim_whitespace=True, header=None, names=colnames)
-ga_df = pd.read_csv(fname_ga, delim_whitespace=True, header=None, names=colnames_ga)
-sa_df = pd.read_csv(fname_sa, delim_whitespace=True, header=None, names=colnames)
+mc_df = pd.read_csv(fname_mc, delim_whitespace=True, header=None, names=colnames, usecols=[0,1,2,3,4,5,6,7,8,9,10])
+ga_df = pd.read_csv(fname_ga, delim_whitespace=True, header=None, names=colnames_ga, usecols=[0,1,2,3,4,5,6,7,8])
+sa_df = pd.read_csv(fname_sa, delim_whitespace=True, header=None, names=colnames_sa, usecols=[0,1,2,3,4,5,6,7,8])
 
 """
  Get info out
 """
 
 #dry_peat_vol_no_dams = 40191.730578848255 # normalization value OLD
-dry_peat_vol_no_dams = 7650.495525801664 # normalization value after 3 days. 2 December
+#dry_peat_vol_no_dams = 7650.495525801664 # normalization value after 3 days. 2 December
+dry_peat_vol_no_dams = 8255.131496485912 # normalization value after 3 days. 6 December. Correct peat depth
 number_dams = (5,10,20, 30, 40, 50, 60, 70, 80)
 
 mc_df = mc_df[mc_df.i != 0]
-mc_plot_vol = mc_df[mc_df['niter']==2000]
-mc_plot_vol = mc_plot_vol[mc_plot_vol['days']==3]
+mc_plot_vol = mc_df[mc_df['days']==3]
 mean_mc_plot_vol = [mc_plot_vol[mc_plot_vol.ndams == i]['dry_peat_vol'].mean()/dry_peat_vol_no_dams*100 for i in number_dams]
 max_mc_plot_vol = [mc_plot_vol[mc_plot_vol.ndams == i]['dry_peat_vol'].max()/dry_peat_vol_no_dams*100 for i in number_dams]
 min_mc_plot_vol = [mc_plot_vol[mc_plot_vol.ndams == i]['dry_peat_vol'].min()/dry_peat_vol_no_dams*100 for i in number_dams]
 
-sa_df = sa_df.rename(columns=rename_cols_sa)
+
 sa_df.dry_peat_vol = sa_df.dry_peat_vol/dry_peat_vol_no_dams*100
 
-ga_df = ga_df.rename(columns=rename_cols_ga)
 ga_df.dry_peat_vol = ga_df.dry_peat_vol/dry_peat_vol_no_dams*100
-
-# slice out
-ga_df = ga_df[21:]
-sa_df = sa_df[sa_df['dry_peat_vol'] < 100]
 
 
 # Choose to plot ndams from number_dams above
 sa_plot = sa_df.loc[sa_df['ndams'].isin(number_dams)]
 ga_plot = ga_df.loc[ga_df['ndams'].isin(number_dams)]
-
-
-sa_plot = sa_plot[sa_plot['day_month']==30]
 
 
 """
@@ -82,11 +74,9 @@ plt.legend()
 
 
 #sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
-# Draw a nested violinplot and split the violins for easier comparison
-#sns.violinplot(x=mc_df.ndams, y=mc_df.dry_peat_vol/dry_peat_vol_no_dams,  inner="quart")
-
+#sns.catplot(x='ndams', y='dry_peat_vol', kind='violin', color='red', data=mc_plot_vol)
 #sns.despine(left=True)
+
 """
 Plot relative improvement
 """
@@ -134,11 +124,18 @@ ax.set_ylabel('Volume fraction of dry peat (%)')
 """
 Plot correlation Vdry peat vs CWL change
 """
+# SA and GA computed one by one from block configurations
+ga_cwl = np.array([100.17000408172562, 206.5699918508545, 294.7300499916108, 413.46002569199203, 490.95001428128023, 611.140032672881, 630.9599639892541, 655.8200357675498, 741.6799679756032])
+sa_cwl = np.array([88.14001469612094, 154.89002861976618, 239.2000149965306, 335.2400191068692, 335.7899886608159, 510.699984335907, 473.95000741482465, 545.1500374794063, 583.9700692176839])
 mc_df3 = mc_df2[mc_df2['days'] == 3]
+
 fig, ax = plt.subplots(1)
-ax.scatter(x=-mc_df3.water_changed_canals, y=mc_df3.dry_peat_vol)
+ax.scatter(x=mc_df3.water_changed_canals, y=mc_df3.dry_peat_vol/dry_peat_vol_no_dams*100, color='pink', alpha=0.7, s=1.5, label='random')
+ax.scatter(x=sa_cwl, y=np.sort(sa_plot.dry_peat_vol)[::-1], color='blue', alpha=0.5, label='SA')
+ax.scatter(x=ga_cwl, y=np.sort(ga_plot.dry_peat_vol)[::-1], color='orange', alpha=0.8, label='GA')
 ax.set_xlabel('CWL change')
 ax.set_ylabel('Volume fraction of dry peat (%)')
+plt.legend()
 
 
 plt.show()
